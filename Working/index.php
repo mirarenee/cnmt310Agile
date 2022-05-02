40bfa50d2d5ab9133ae46b469c49bfd60d0f1aa2
bkoski310 <98343223+bkoski310@users.noreply.github.com>
2022-05-01 20:04:14 -0500

    What I got working on my web server

---
+++ Working/index.php
@@ -0,0 +1,89 @@
+<?php
+require_once("Template.php");
+require_once("WebServiceClient.php");
+require_once("const.php");
+$client = new WebServiceClient("http://cnmt310.braingia.org/wwsp/playlist/");
+
+$page = new Template("Playlist");
+$page->addHeadElement("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>");
+$page->addHeadElement('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>');
+$page->addHeadElement('<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>');
+$page->addHeadElement("<script src='javascript.js'></script>");
+$page->addHeadElement("<script src='testac.js?" .time() . "'></script>");
+$page->finalizeTopSection();
+
+$page->finalizeBottomSection();
+
+print $page->getTopSection();
+
+print "<br />\n";
+print "<div class='position-relative'>\n";
+print "<div class='mx-auto' style='width: 200px;'>\n";
+print "<header>\n";
+print "<h2>Playlist</h2>\n";
+print "</header>\n";
+print "</div>\n";
+
+print "<br /><br />\n";
+
+print "<div class='mx-auto' style='width: 500px;'>\n";
+print "<form action='#' method='POST'>\n";
+print "<label for='searchTitle' class='form-label'>Song List</label>\n";
+print "<input class='form-control' id='form-field' placeholder='Type to search...'>\n";
+print "</form>\n";
+print "</div>\n";
+
+print "<br /><br /><br />\n";
+
+
+/** replace this section with data pulled from the auto complete
+$action = "playlist_get";
+$data = array("apikey" => "bcljigpion",	//REPLACE WITH APIKEY WHEN CONST IS WORKING
+              "apiuser" => "api60", //APIUSER,
+              "action" => $action,
+              );
+$client->setPostFields($data);
+
+$result = json_decode($client->send());
+
+**/
+print "<div class='mx-auto' style='width: 500px;'>\n";
+print "<form method=\"POST\" action=\"playlistAdd.php\" onsubmit=\"validate()\">";
+print "<label for=\"title\" class=\"required\">Title:</label>";
+print "<input type=\"text\" id=\"title\" name=\"title\"><br />\n";
+print "<label for=\"artist\" class=\"required\">Artist:</label>";
+print "<input type=\"text\" id=\"artist\" name=\"artist\"><br />\n";
+print "<input type=\"submit\" name=\"submit\" value=\"Submit\">\n";
+print "<input type=\"reset\" name=\"reset\" value=\"Reset\">\n";
+print "</form>\n";
+print "</div>\n";
+
+//Displays the playlist for today
+$client->setMethod("GET");
+$action = "playlist_get";
+$data = array("apikey" => APIKEY,
+              "apiuser" => APIUSER,
+              "action" => $action,
+              );
+$client->setPostFields($data);
+
+$result = json_decode($client->send());
+print "<div class='mx-auto' style='width: 500px;'>\n";
+print "<table>";
+foreach ($result as $key => $row) {
+	if ($row == "Error")
+	{
+		print "No songs in the playlist";
+		break;
+	}
+	print "<tr>";
+	print "<td>" . $row->title . "</td>";
+	print "<td>" . $row->artist . "</td>";
+	print "<td>" . $row->album . "</td>";
+	print "<td>" . $row->category . "</td>";
+	print "</tr>";
+}
+print "</table>";
+print "</div>\n";
+
+print "</div>\n";
---
+++ Working/testac.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$( "#form-field" ).autocomplete({
+		minLength: 2,
+		source: function (request, response) {
+			$.ajax({
+				url: "https://cnmt310.braingia.org/wwsp/song/index.php",
+				type: "POST",
+				dataType: "json",
+				data: {
+					term: request.term,
+					apikey: "bcljigpion",
+					apiuser:"api60"
+				},
+				success: function (data) {
+					if (data.result == "Error") {
+						return;
+					}
+				
+					console.log(data); //REMOVE THIS FOR FINAL PRODUCT
+					response ($.map(data, function (item) {
+					let title = item.title + " - " + item.artist + " - " + item.album;
+					let value = item.title;
+					return { label: title, data: item }
+				}));
+				}
+			});
+		},
+		minLength: 2,
+		select: function(event,ui) {
+			console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
+		} 
+    });
+} );
