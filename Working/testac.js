87de58bf86ec57b240e245f645f484a0f20bfa71
bkoski310 <98343223+bkoski310@users.noreply.github.com>
2022-05-02 11:07:14 -0500

    autofill

--- Working/testac.js
+++ Working/testac.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$( "#form-field" ).autocomplete({
+	$( "#title" ).autocomplete({
 		minLength: 2,
 		source: function (request, response) {
 			$.ajax({
@@ -9,16 +9,16 @@ $(document).ready(function() {
 				data: {
 					term: request.term,
 					apikey: "bcljigpion",
-					apiuser:"api60"
+					apiuser: "api60"
 				},
 				success: function (data) {
 					if (data.result == "Error") {
 						return;
 					}
 				
-					console.log(data); //REMOVE THIS FOR FINAL PRODUCT
+//					console.log(data); //REMOVE THIS FOR FINAL PRODUCT
 					response ($.map(data, function (item) {
-					let title = item.title + " - " + item.artist + " - " + item.album;
+					let title = item.title;
 					let value = item.title;
 					return { label: title, data: item }
 				}));
@@ -27,7 +27,38 @@ $(document).ready(function() {
 		},
 		minLength: 2,
 		select: function(event,ui) {
-			console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
+//			console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
+		} 
+    });
+	$( "#artist" ).autocomplete({
+		minLength: 2,
+		source: function (request, response) {
+			$.ajax({
+				url: "https://cnmt310.braingia.org/wwsp/song/index.php",
+				type: "POST",
+				dataType: "json",
+				data: {
+					term: request.term,
+					apikey: "bcljigpion",
+					apiuser: "api60"
+				},
+				success: function (data) {
+					if (data.result == "Error") {
+						return;
+					}
+				
+//					console.log(data); //REMOVE THIS FOR FINAL PRODUCT
+					response ($.map(data, function (item) {
+					let artist = item.artist;
+					let value = item.artist;
+					return { label: artist, data: item }
+				}));
+				}
+			});
+		},
+		minLength: 2,
+		select: function(event,ui) {
+//			console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
 		} 
     });
 } );
