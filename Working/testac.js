5adfafe02ebad1792bff4cda6489d4d93497d9df
Prince <cprin799@uwsp.edu>
2022-05-12 22:42:12 -0500

    autocomplete fixed?

--- Working/testac.js
+++ Working/testac.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
 					if (data.result == "Error") {
 						return;
 					}
-					//console.log(data); //REMOVE THIS FOR FINAL PRODUCT
+					console.log(data); //REMOVE THIS FOR FINAL PRODUCT
 					response ($.map(data, function (item) {
-					let title = item.title + " - " + item.artist + " - " + item.album;
+					let title = item.title + " - " + item.artist + " - " + item.album + " - " + item.category;
 					let value = item.title
 					return { label: title, data: item }
 				}));
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		},
 		minLength: 2,
 		select: function(event,ui) {
-		//console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
+		console.log(ui); //REMOVE THIS FOR FINAL PRODUCT
 		}
     });
 } );
